6464b4e2af68b87424b9b0c6210d5f4f4c81a4fb
tzhd427 <115906803+xiaoniao427@users.noreply.github.com>
2026-07-26 15:59:34 +0800

    修改开发者

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/path/UrlManager.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/path/UrlManager.kt
@@ -51,7 +51,7 @@ const val URL_PROJECT: String = "https://github.com/ZalithLauncher/ZalithLaunche
 const val URL_PROJECT_INFO: String = "https://api.github.com/repos/ZalithLauncher/Zalith-Info/contents/v2"
 const val URL_COMMUNITY: String = "https://github.com/ZalithLauncher/ZalithLauncher2/graphs/contributors"
 const val URL_WEBLATE: String = "https://hosted.weblate.org/projects/zalithlauncher2"
-const val URL_SUPPORT: String = "https://ifdian.net/a/MovTery"
+const val URL_SUPPORT: String = "https://ifdian.net/a/tzhd427"
 const val URL_EASYTIER: String = "https://easytier.cn/"
 
 const val URL_GITHUB_RENDERER_PLUGINS = "https://github.com/ShirosakiMio/FCLRendererPlugin/releases/tag/Renderer"
